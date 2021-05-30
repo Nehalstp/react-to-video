241ea9058f52433fe1e12211a5cfba5914a04cc4
Nehalstp <68918385+Nehalstp@users.noreply.github.com>
2021-05-30 21:27:30 +0530

    first commit

---
+++ videokyc/build/precache-manifest.becc78e4efc41b0b5c3176507266fcdd.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a2e2ea58a8fd85973267c08526959e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b41f9f3b13cbe7b3cba6",
+    "url": "/static/css/2.c301f1a7.chunk.css"
+  },
+  {
+    "revision": "0facf41a0b0b6e43f948",
+    "url": "/static/css/main.0c9f944c.chunk.css"
+  },
+  {
+    "revision": "b41f9f3b13cbe7b3cba6",
+    "url": "/static/js/2.27e81e2f.chunk.js"
+  },
+  {
+    "revision": "aed6692713e2724a8cb741a0ef419d81",
+    "url": "/static/js/2.27e81e2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0facf41a0b0b6e43f948",
+    "url": "/static/js/main.7e938d19.chunk.js"
+  },
+  {
+    "revision": "3c733e2be163dc669b4b",
+    "url": "/static/js/runtime-main.90ec628a.js"
+  },
+  {
+    "revision": "f751c5307aac8b71e154e4c30476619c",
+    "url": "/static/media/Instruction_1.f751c530.png"
+  },
+  {
+    "revision": "61f7c6fd3b38bacf62edff5bae832eb5",
+    "url": "/static/media/Instruction_2.61f7c6fd.png"
+  },
+  {
+    "revision": "034a7782ff6935acca12bb8ca0ee9776",
+    "url": "/static/media/Instruction_3.034a7782.png"
+  },
+  {
+    "revision": "5a44265b37640f0520a6fccb5e76a91a",
+    "url": "/static/media/Login-bgImg.5a44265b.jpg"
+  },
+  {
+    "revision": "5ffc0f9873321f04be4a545e5277fcc5",
+    "url": "/static/media/RecordOff.5ffc0f98.svg"
+  },
+  {
+    "revision": "ee21215af0c62ddd4695c64e54189ee2",
+    "url": "/static/media/RecordOn.ee21215a.svg"
+  },
+  {
+    "revision": "3caf50e1b99e7f868631bf96b65ba02b",
+    "url": "/static/media/Untitled-1.3caf50e1.png"
+  },
+  {
+    "revision": "5a1fe3dcafc6798eeb3af3ee7d2573e0",
+    "url": "/static/media/bg.5a1fe3dc.png"
+  },
+  {
+    "revision": "826c4d51b1f31417149c6a16ee973e9a",
+    "url": "/static/media/camera_shutter.826c4d51.mp3"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "5ab106c21fc9130c706de80a41f0f103",
+    "url": "/static/media/testimony.5ab106c2.png"
+  }
+]);
